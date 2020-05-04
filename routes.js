4b5e375d2cbc25a82bcde83cc030eaa5e840f7d3
Bilqist <bilqistaisaah@gmail.com>
2020-05-04 20:54:53 +0700

    tampil

--- routes.js
+++ routes.js
@@ -3,4 +3,17 @@ module.exports=function(app){
     var jsonku = require('./controller');
     app.route('/')
     .get(jsonku.index);
+
+    app.route('/tampil')
+    .get(jsonku.tampilsemuamontir);
+
+    app.route('/tampilsp')
+    .get(jsonku.tampilsemuasparepart);
+
+    app.route('/tampil/:id')
+    .get(jsonku.tampilberdasarkanidmontir);
+
+    app.route('/tampilsp/:id')
+    .get(jsonku.tampilberdasarkanidsparepart);
+
 }
