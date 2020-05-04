38daf414f823ea4579b23a8db40ff82ee302fee9
Bilqist <bilqistaisaah@gmail.com>
2020-05-05 00:01:45 +0700

    verifikasi

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
+var verifikasi = require('./verifikasi');
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
